e3c1d9b06ca32cdb204879a2f0d62ce6df7485ff
oyaken0717 <oyaken0717@yahoo.co.jp>
2022-03-23 16:12:19 +0900

    ファビコンの追加

--- src/main/java/com/example/controller/InsideLineContoller.java
+++ src/main/java/com/example/controller/InsideLineContoller.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
